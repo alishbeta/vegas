1bc9d39a99fd04d30724dc0a003d982dec992990
DimaS <Dima.simonchuk@gmail.com>
2018-09-07 16:23:38 +0300

    cat.html fix

--- template/js/init.js
+++ template/js/init.js
@@ -153,7 +153,7 @@ $(document).ready(function () {
         $("#slider-" + data.filterNumber + "-a2").val($("#slider-" + data.filterNumber + "-r").slider("values", 1));
     });
 
-    $('.filters-wrap .vg-filter').on('click', function () {
+    $('.filters-wrap .btn-display').on('click', function () {
         $('.filters-data').toggleClass('open');
         $('.f1').toggleClass('open');
         $('.prod-wrap').toggleClass('short');
