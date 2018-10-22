10b8efe188ef1630a71a7a66e4f1c5afcc3bc219
DimaS <Dima.simonchuk@gmail.com>
2018-10-22 14:49:23 +0300

    add maps

--- template/js/maps.js
+++ template/js/maps.js
@@ -1,7 +1,8 @@
 var icomsBase = 'img/';
 var map;
+var adresses = $('#address').data('address');
 function initMap() {
-    //console.log(points.kiev);
+    geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 50.458961,
@@ -10,51 +11,39 @@ function initMap() {
         zoom: 11
     });
 
-    var pos = [
-        new google.maps.LatLng(50.458961, 30.337648),
-        new google.maps.LatLng(50.362884, 30.468250),
-        new google.maps.LatLng(50.460908, 30.643279),
-        new google.maps.LatLng(50.418068, 30.543042),
-        new google.maps.LatLng(50.438158, 30.357544),
-        new google.maps.LatLng(50.514390, 30.497615),
-        new google.maps.LatLng(50.460541, 30.646087),
-    ];
+    adresses.forEach(addPoint);
 
-    var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    '(last visited June 22, 2009).</p>'+
-    '</div>'+
-    '</div>';
+    function addPoint(data, index) {
+        geocoder.geocode({ 'address': data.addr }, function (results, status) {
+            if (status == 'OK') {
+                console.log(data.addr);
+                console.log(results[0]);
 
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: icomsBase + 'map_logo.png',
+                    position: results[0].geometry.location,
+                    title: 'Bed4you'
+                });
 
-    
-    pos.forEach(function (pos) {
-        var marker = new google.maps.Marker({
-            position: pos,
-            icon: icomsBase + 'map_logo.png',
-            map: map,
-            title: 'Bed4you'
+                var contentString = '<div id="content">' +
+                    '<div id="siteNotice">' +
+                    '</div>' +
+                    '<h3 id="firstHeading" class="firstHeading">' + results[0].address_components[3].long_name + '</h1>' +
+                    data.description +
+                    '<div id="bodyContent">' +
+                    '</div>' +
+                    '</div>';
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
+            } else {
+                console.log('Geocode was not successful for the following reason: ' + status);
+            }
         });
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-          });
-    });
+    }
 }
