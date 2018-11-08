6378524c12fff65effe81785374085f047fdc96c
sasha <ks@mo-apps.com>
2018-11-08 18:23:57 +0200

    #247

---
+++ src/Presentation/Nop.Web/Components/CatalogFiltersSelector.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+using Nop.Web.Models.Catalog;
+
+namespace Nop.Web.Components
+{
+    public class CatalogFiltersSelectorViewComponent : NopViewComponent
+    {
+        private readonly ICatalogModelFactory _catalogModelFactory;
+        private readonly IWebHelper _webHelper;
+
+        public CatalogFiltersSelectorViewComponent(ICatalogModelFactory catalogModelFactory, 
+               IWebHelper webHelper)
+        {
+            this._catalogModelFactory = catalogModelFactory;
+            this._webHelper = webHelper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            FilterRangesModel model = new FilterRangesModel();
+            if (!string.IsNullOrEmpty(_webHelper.QueryString<string>("price")))
+            {
+                model.Price = new FilterRangesModel.FilterRange()
+                {
+                    from = double.Parse(_webHelper.QueryString<string>("price").Split('-')[0]),
+                    to = double.Parse(_webHelper.QueryString<string>("price").Split('-')[1])
+                };
+            }
+            if (!string.IsNullOrEmpty(_webHelper.QueryString<string>("height")))
+            {
+                model.Height = new FilterRangesModel.FilterRange()
+                {
+                    from = double.Parse(_webHelper.QueryString<string>("height").Split('-')[0]),
+                    to = double.Parse(_webHelper.QueryString<string>("height").Split('-')[1])
+                };
+            }
+            if (!string.IsNullOrEmpty(_webHelper.QueryString<string>("length")))
+            {
+                model.Length = new FilterRangesModel.FilterRange()
+                {
+                    from = double.Parse(_webHelper.QueryString<string>("length").Split('-')[0]),
+                    to = double.Parse(_webHelper.QueryString<string>("length").Split('-')[1])
+                };
+            }
+            if (!string.IsNullOrEmpty(_webHelper.QueryString<string>("width")))
+            {
+                model.Width = new FilterRangesModel.FilterRange()
+                {
+                    from = double.Parse(_webHelper.QueryString<string>("width").Split('-')[0]),
+                    to = double.Parse(_webHelper.QueryString<string>("width").Split('-')[1])
+                };
+            }
+            if (!string.IsNullOrEmpty(_webHelper.QueryString<string>("sleeplength")))
+            {
+                model.SleepLength = new FilterRangesModel.FilterRange()
+                {
+                    from = double.Parse(_webHelper.QueryString<string>("sleeplength").Split('-')[0]),
+                    to = double.Parse(_webHelper.QueryString<string>("sleeplength").Split('-')[1])
+                };
+            }
+            if (!string.IsNullOrEmpty(_webHelper.QueryString<string>("sleepwidth")))
+            {
+                model.SleepWidth = new FilterRangesModel.FilterRange()
+                {
+                    from = double.Parse(_webHelper.QueryString<string>("sleepwidth").Split('-')[0]),
+                    to = double.Parse(_webHelper.QueryString<string>("sleepwidth").Split('-')[1])
+                };
+            }
+            return View(model);
+        }
+    }
+}
---
+++ src/Presentation/Nop.Web/Models/Catalog/FilterRangesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Models.Catalog
+{
+    public class FilterRangesModel
+    {
+        public FilterRange Height { get; set; }
+        public FilterRange Width { get; set; }
+        public FilterRange Length { get; set; }
+        public FilterRange SleepLength { get; set; }
+        public FilterRange SleepWidth { get; set; }
+        public FilterRange Price { get; set; }
+        public class FilterRange
+        {
+            public double from { get; set; }
+            public double to { get; set; }
+        }
+    }
+}
