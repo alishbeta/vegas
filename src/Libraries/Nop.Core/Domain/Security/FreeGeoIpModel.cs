7cfb514d0eb1468003cd5bd460f298ee47a8238c
Dmitry <smithsdmitry@gmail.com>
2018-11-15 22:44:29 +0200

    Ceoloation added.

---
+++ src/Libraries/Nop.Core/Domain/Security/FreeGeoIpModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Core.Domain.Security
+{
+    public class FreeGeoIpModel
+    {
+        public string ip { get; set; }
+        public string country_code { get; set; }
+        public string country_name { get; set; }
+        public string region_code { get; set; }
+        public string region_name { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public double? latitude { get; set; }
+        public double? longitude { get; set; }
+        //public int metro_code { get; set; }
+        //{
+        //  "ip": "5.9.13.82",
+        //  "country_code": "DE",
+        //  "country_name": "Germany",
+        //  "region_code": "",
+        //  "region_name": "",
+        //  "city": "",
+        //  "zip_code": "",
+        //  "time_zone": "",
+        //  "latitude": 51.2993,
+        //  "longitude": 9.491,
+        //  "metro_code": 0
+        //}
+    }
+}
---
+++ src/Libraries/Nop.Services/Security/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using Nop.Core.Domain.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Services.Security
+{
+    public interface ILocationService
+    {
+        FreeGeoIpModel GetLocation(string ip);
+    }
+}
