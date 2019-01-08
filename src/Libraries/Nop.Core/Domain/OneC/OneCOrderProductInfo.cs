ad0601c17a42478727bdc842b4f96f5a25195e9a
Dmitry <smithsdmitry@gmail.com>
2019-01-08 22:28:37 +0200

    Counts of product added.

--- src/Libraries/Nop.Core/Domain/OneC/OneCOrderProductInfo.cs
+++ src/Libraries/Nop.Core/Domain/OneC/OneCOrderProductInfo.cs
@@ -9,5 +9,6 @@ namespace Nop.Core.Domain.OneC
         public string ProduxtSku { get; set; }
         public decimal Price { get; set; }
         public decimal DiscountPrice { get; set; }
+        public int Quantity { get; set; }
     }
 }
