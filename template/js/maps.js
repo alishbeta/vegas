be5898c0331f3ff5043c9cbedf407d333f211468
DimaS <Dima.simonchuk@gmail.com>
2019-03-01 11:04:35 +0200

    Фиксик

--- template/js/maps.js
+++ template/js/maps.js
@@ -11,7 +11,7 @@ function initMap() {
     } else {
         if ($('*').is('#map') && adresses != undefined) {
             geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ 'address': data.city }, function (results, status) {
+            geocoder.geocode({ 'address': adresses[0].city }, function (results, status) {
                 if (status == 'OK') {
                     map = new google.maps.Map(document.getElementById('map'), {
                         center: results[0].geometry.location,
